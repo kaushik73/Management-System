a5dcf9cc4149c07e1ad9b00d9494dfb44cb2cea6
Gyanendra Kumar <gyanendra.kumar@intimetec.com>
2024-09-11 14:08:17 +0530

    Resolved comments and implement custom input tas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 // import React from "react";
 import { useSelector } from "react-redux";
-import { FaBeer } from "react-icons/fa";
+import Input from  "../wrapperTags/Input";
+import Heading from "../wrapperTags/Heading";
+import Button from "../wrapperTags/Button";
 
 const Home = () => {
   const theme = useSelector((store) => store.config.theme);
@@ -9,13 +11,9 @@ const Home = () => {
      <div className="min-h-screen bg-gradient-to-b from-blue-600 to-purple-600">
             {/* Header Section */}
             <header className="text-center py-20">
-                <h1 className="text-4xl font-bold text-white">Hi, how can we help you?</h1>
+                <Heading type="h1" data="Hi, how can we help you?" className="text-4xl font-bold text-white"/>
                 <div className="mt-8">
-                <input
-                    type="text"
-                    placeholder="Search for solutions, services and tickets"
-                    className="px-4 py-3 w-96 rounded-lg shadow-md focus:outline-none"
-                />
+                <Input/>
                 </div>
             </header>
 
@@ -24,13 +22,11 @@ const Home = () => {
                 <div className="w-3/4 bg-indigo-800 text-white p-6 rounded-lg shadow-md">
                 <div className="flex justify-between items-center">
                     <div>
-                    <h2 className="text-lg font-semibold">Welcome to Management System - India</h2>
+                    <Heading type="h2" data="Welcome Action Items - India"/>
                     <p className="text-sm text-gray-300">Made with Love</p>
                     </div>
                     <div>
-                    <button className="bg-white text-indigo-800 py-2 px-4 rounded-md shadow hover:bg-gray-100">
-                        View all
-                    </button>
+                      <Button size="large"/>
                     </div>
                 </div>
                 </div>
--- src/utils/constansts.js
+++ src/utils/constansts.js
@@ -4,3 +4,5 @@ export const GET_API_OPTIONS = {
     accept: "application/json",
   },
 };
+
+export const LOGO_URL = "https://projectsly.com/images/blog/employee-task-management-system.png?v=1686553999071005322";
